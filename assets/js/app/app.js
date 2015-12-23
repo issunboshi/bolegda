11f6679584c8070837580ad0ac400b88deb44fbd
Cliff Williams <cliff@hiddenpause.com>
2015-12-23 23:17:32 +0000

    Small tweaks

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,14 +1,20 @@
 import 'howler';
 import Vue from 'vue';
 
+import removeClass from 'helpers/removeClass';
+
 var sound = new Howl({
-  urls: ['/assets/audio/bolegda-full.mp3'],
-  sprite: {
-      full: [0, 600],
-      ba: [0, 219],
-      le: [220, 260],
-      gda: [370, 450]
-  }
+    urls: ['/assets/audio/bolegda-full.mp3'],
+    sprite: {
+        full: [0, 600],
+        ba: [0, 219],
+        le: [220, 260],
+        gda: [370, 450]
+    },
+    onload: function () {
+        debugger;
+        removeClass(document.querySelector('.js-audio-controls'), '-inactive');
+    }
 });
 
 var AudioControls = new Vue({
